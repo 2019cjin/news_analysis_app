d497c8358019eaa230ec36f785d14165caa3bfdb
Caroline <cj.jin@hotmail.com>
2020-07-02 15:35:55 -0400

    Open windows search bar

--- headlines/static/headlines/autocomplete_generator.js
+++ headlines/static/headlines/autocomplete_generator.js
@@ -1,6 +1,4 @@
-"use strict";
-function open_url(textbox, url_ref) {
-			var word = textbox.value;
-			var new_url = url.replace("foobar", word);
-			window.open(new_url);
-		};
+'use strict';
+function myFunction() {
+	alert("Hello World!");
+}
